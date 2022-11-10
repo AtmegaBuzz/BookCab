549cb13583d648f94835d58299e8f26a968bd4d5
AtmegaBuzz <swapnilshinde9382@gmail.com>
2022-11-10 22:03:15 +0530

    created auth pages and views

--- backend/auth.py
+++ backend/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session, request
+from flask import Blueprint, render_template, session, request, redirect
 
 
 auth = Blueprint("auth",__name__)
@@ -6,8 +6,35 @@ auth = Blueprint("auth",__name__)
 @auth.route("/register",methods=["POST","GET"])
 def register():
 
-    if request.method == 'GET':
-        return render_template('register.html')
+    user = session.get("user",None)
+    if user!=None:
+        return redirect("/home")
 
-    if request.method == 'POST': 
-        return render_template('register.html')
+    if request.method == "POST":
+        
+        print(request.form)
+
+
+        
+    return render_template(
+        'register.html',
+        user = user
+    )
+
+@auth.route('/login',methods=['POST','GET'])
+def login():
+
+    user = session.get("user",None)
+    if user!=None:
+        return redirect("/home")
+
+    if request.method == "POST":
+        
+        print(request.form)
+
+
+        
+    return render_template(
+        'login.html',
+        user = user
+    ) 
