d7569647e37470d6c61532b4d28a64c90d2a1fab
AtmegaBuzz <swapnilshinde9382@gmail.com>
2022-11-11 13:00:06 +0530

    create cost calculator

--- backend/algorithm.py
+++ backend/algorithm.py
@@ -117,7 +117,19 @@ class User:
 
 
 class UserAttributes:
-    pass
+
+    def __init__(self, userid, destination, booking_id ,distance, time, cost):
+        self.userid = userid
+        self.destination = destination
+        self.booking_id = booking_id
+        self.distance = distance
+        self.time = time
+        self.cost = cost
+
+    def to_string(self):
+        return str(self.userid) + " " + self.destination + " " + str(self.distance) + " " + str(self.time) + " " + str(self.cost)
+
+
 
 cache = dict()
 users = list()
@@ -134,11 +146,52 @@ if not home_source:
     source = 'rajiv chowk'
 
 def solve(taxino, curloc, curtime, users_in_taxi, cost, best_config):
-    pass
+
+    global answer
+    if users_in_taxi.count(0) + users_in_taxi.count(1) == 0:
+        if cost < answer:
+            best_config.clear()
+            for i in users_in_taxi:
+                best_config.append(i)
+            answer = cost
+        return 0
+    if dpstate(taxino, curloc, curtime, users_in_taxi) in cache:
+       return cache[dpstate(taxino, curloc, curtime, users_in_taxi)]
+
+    res = 1e9
+
+    # Move on to new taxi if all passengers have reached their destination
+    if curloc != home_source and users_in_taxi.count(1) == 0:
+        return solve(taxino + 1, home_source, 0, users_in_taxi, cost, best_config)
+
+    # Try picking up passengers if there is space and we are at source and calculate best result
+    picked_up = users_in_taxi.count(1)
+    for i in range(0, n):
+
+        if picked_up < 4 and users_in_taxi[i] == 0 and curloc == home_source:
+            users_in_taxi[i] = 1
+            res = min(res, solve(taxino, curloc, curtime, users_in_taxi, cost, best_config))
+            users_in_taxi[i] = 0
+
+    # Try dropping off passengers and see if it yields best result
+    for i in range(0, n) :
+
+        if users_in_taxi[i] == 1:
+            users_in_taxi[i] = taxino
+            res = min(res, get_distance(curloc, users[i].destination) + solve(taxino, users[i].destination, curtime + get_distance(curloc, users[i].destination), users_in_taxi, cost + get_distance(curloc, users[i].destination), best_config))
+            users_in_taxi[i] = 1
+
+    cache[dpstate(taxino, curloc, curtime, users_in_taxi)] = res
+    
+    return res
 
 
 def cost_calculator(dist, tot_dist, min_cost, tot_cost) :
-    pass
+    val = (1.0 * dist) / (1.0 * tot_dist)
+    val *= tot_cost
+    val = math.floor(val)
+    val = max(val, min_cost)
+    return val/10
 
 
 def print_group_pattern(users_in_taxi) :
