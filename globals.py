6d0016d5e3ff4c3dbaddd56ccebb3ce1079b716b
AtmegaBuzz <swapnilshinde9382@gmail.com>
2022-11-11 17:09:24 +0530

    organization source is transfered to .env

--- globals.py
+++ globals.py
@@ -1,5 +1,5 @@
-import os
 import googlemaps
+import os
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
@@ -11,13 +11,17 @@ TEMPLATES_URI = os.path.join(os.getcwd(),"templates")
 DB_NAME = "database.db"
 UPLOAD_FOLDER = "static/images"
 SECRET_KEY = os.getenv("SECRET_KEY",None)
+SOURCE = os.getenv("ORG_SOURCE",None)
+maps_apikey = os.getenv("GOOGLE_MAPS_APIKEY",None)
+
+if SOURCE == None:
+    print("ORG_SOURCE is required in .env")
 
 if SECRET_KEY == None:
     print("Secret key is required in .env")
     os._exit(0)
 
 
-maps_apikey = os.getenv("GOOGLE_MAPS_APIKEY",None)
 if maps_apikey == None:
     print("Missing google maps api key in .env")
     os._exit(0)
